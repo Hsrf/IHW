738ce9147e712e3f71f8ab6e7941f20da9ed55de
joaopedro-jordao <48602401+joaopedro-jordao@users.noreply.github.com>
2019-05-05 16:11:36 -0300

    Update B.v

--- B.v
+++ B.v
@@ -1,13 +1,13 @@
 module B(
-	input wire [15:0]data2,
+	input wire [31:0]data2,
 	input wire writeRegB,
-	output wire [15:0]valueB
+	output wire [31:0]valueB
 	);
 	
 always @(posedge clock or negedge reset)
 begin
  if(clock)
 	if(writeRegB == 1)
-		valueB = data2
+		valueB <= data2
 end
 endmodule
