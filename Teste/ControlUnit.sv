a47a596cf1497234a00ecab4478fa7b15dd6d84b
gabrielaheimer <38387611+gabrielaheimer@users.noreply.github.com>
2019-05-17 09:27:19 -0300

    sra

--- Teste/ControlUnit.sv
+++ Teste/ControlUnit.sv
@@ -50,7 +50,12 @@ enum logic [6:0] {
 	Sll = 7'd17,
 	SllWriteReg = 7'd18,
 	Sllv = 7'd19,
-	SllvWriteReg = 7'd20
+	SllvWriteReg = 7'd20,
+	Sra = 7'd21,
+	SraOp = 7'd22,
+	SraWriteReg = 7'd23,
+	SllvOp = 7'd24,
+	SllOp = 7'd25
 } state, nextstate;
 	
 always_ff@(posedge clock, posedge reset) begin
@@ -176,6 +181,7 @@ always @* begin
 						6'h13: nextstate = Rte;
 						6'h0: nextstate = Sll;
 						6'h4: nextstate = Sllv;
+						6'h3: nextstate = Sra;
 					endcase
 				end
 				
@@ -285,6 +291,27 @@ always @* begin
 				ShiftControl = 3'd1;
 				ShiftSrc = 1'd1;
 				ShiftAmt = 1'd1;
+				nextstate = SllOp;
+		end
+		SllOp: begin
+				ALUSrcA = 2'd0;
+				ALUSrcB = 3'd0;
+				PCSource = 3'd0;
+				ALUOp = 3'd0;
+				PCWrite = 1'd0;
+				MemWr = 1'd0;
+				IRWrite = 1'd0;
+				Iord = 3'd0;
+				MemToReg = 4'd0;
+				WriteRegA = 1'd0;
+				WriteRegB = 1'd0;
+				ALUOutControl = 1'd0;
+				RegDst = 2'd0;
+				RegWrite = 1'd0;
+				EPCWrite = 1'd0;
+				ShiftControl = 3'd2;
+				ShiftSrc = 1'd1;
+				ShiftAmt = 1'd1;
 				nextstate = SllWriteReg;
 		end
 		SllWriteReg: begin
@@ -327,6 +354,27 @@ always @* begin
 				ShiftControl = 3'd1;
 				ShiftSrc = 1'd0;
 				ShiftAmt = 1'd0;
+				nextstate = SllvOp;
+		end
+		SllvOp: begin
+				ALUSrcA = 2'd0;
+				ALUSrcB = 3'd0;
+				PCSource = 3'd0;
+				ALUOp = 3'd0;
+				PCWrite = 1'd0;
+				MemWr = 1'd0;
+				IRWrite = 1'd0;
+				Iord = 3'd0;
+				MemToReg = 4'd0;
+				WriteRegA = 1'd1;
+				WriteRegB = 1'd1;
+				ALUOutControl = 1'd0;
+				RegDst = 2'd0;
+				RegWrite = 1'd0;
+				EPCWrite = 1'd0;
+				ShiftControl = 3'd2;
+				ShiftSrc = 1'd0;
+				ShiftAmt = 1'd0;
 				nextstate = SllvWriteReg;
 		end
 		SllvWriteReg: begin
@@ -348,6 +396,69 @@ always @* begin
 				ShiftControl = 3'd2;
 				ShiftSrc = 1'd0;
 				ShiftAmt = 1'd0;
+				nextstate = Wait;
+		end
+		Sra: begin
+				ALUSrcA = 2'd0;
+				ALUSrcB = 3'd0;
+				PCSource = 3'd0;
+				ALUOp = 3'd0;
+				PCWrite = 1'd0;
+				MemWr = 1'd0;
+				IRWrite = 1'd0;
+				Iord = 3'd0;
+				MemToReg = 4'd0;
+				WriteRegA = 1'd0;
+				WriteRegB = 1'd1;
+				ALUOutControl = 1'd0;
+				RegDst = 2'd0;
+				RegWrite = 1'd0;
+				EPCWrite = 1'd0;
+				ShiftControl = 3'd1;
+				ShiftSrc = 1'd1;
+				ShiftAmt = 1'd1;
+				nextstate = SraOp;
+		end
+		SraOp: begin
+				ALUSrcA = 2'd0;
+				ALUSrcB = 3'd0;
+				PCSource = 3'd0;
+				ALUOp = 3'd0;
+				PCWrite = 1'd0;
+				MemWr = 1'd0;
+				IRWrite = 1'd0;
+				Iord = 3'd0;
+				MemToReg = 4'd0;
+				WriteRegA = 1'd0;
+				WriteRegB = 1'd0;
+				ALUOutControl = 1'd0;
+				RegDst = 2'd0;
+				RegWrite = 1'd0;
+				EPCWrite = 1'd0;
+				ShiftControl = 3'd4;
+				ShiftSrc = 1'd1;
+				ShiftAmt = 1'd1;
+				nextstate = SraWriteReg;
+		end
+		SraWriteReg: begin
+				ALUSrcA = 2'd0;
+				ALUSrcB = 3'd0;
+				PCSource = 3'd0;
+				ALUOp = 3'd0;
+				PCWrite = 1'd0;
+				MemWr = 1'd0;
+				IRWrite = 1'd0;
+				Iord = 3'd0;
+				MemToReg = 4'd4;
+				WriteRegA = 1'd0;
+				WriteRegB = 1'd0;
+				ALUOutControl = 1'd0;
+				RegDst = 2'd3;
+				RegWrite = 1'd1;
+				EPCWrite = 1'd0;
+				ShiftControl = 3'd4;
+				ShiftSrc = 1'd1;
+				ShiftAmt = 1'd1;
 				nextstate = Wait;
 		end
 		Rte: begin
